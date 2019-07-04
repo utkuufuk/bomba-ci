c7e2e0361a4dd5297546d8314926c60ab1f783ea
utkuufuk <utkuufuk@gmail.com>
2019-07-04 16:06:24 +0300

    convert 'then' blocks into 'async/await'

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.post(process.env.WEBHOOK_ENDPOINT_SUFFIX!, (req: Request, res: Response) => {
+app.post(process.env.WEBHOOK_ENDPOINT_SUFFIX!, async (req: Request, res: Response) => {
     // make sure that the webhook request is actually made by Github
     if (github.verifyWebhookSignature(req)) {
         return res.status(500).json({message: 'Webhook signature could not be verified.'});
@@ -38,66 +38,49 @@ app.post(process.env.WEBHOOK_ENDPOINT_SUFFIX!, (req: Request, res: Response) => 
 
     // initialize & read CI config
     let cfg: PipelineConfig = {build: []};
-    let promise = exec(`rm -rf ${process.env.WORK_DIR}/repo`)
-        // clone branch
-        .then(() => {
-            const cloneCmd =
-                `git clone --single-branch --branch ` +
-                `${pr.head.ref} ${pr.head.repo.ssh_url} ${process.env.WORK_DIR}/repo`;
-            return exec(cloneCmd);
-        })
-        // load 'github-ci.yml'
-        .then(() => {
-            const file = fs.readFileSync(`${process.env.WORK_DIR}/repo/github-ci.yml`, 'utf8');
-            cfg = yaml.safeLoad(file);
-            return cfg;
-        })
-        // copy env file into project root
-        .then(({env}) =>
-            env === null
-                ? Promise.resolve()
-                : exec(`cp ${process.env.WORK_DIR}/${env} ${process.env.WORK_DIR}/repo/${env}`)
-        )
-        // set all status checks as 'pending'
-        .then(() => github.sendStatusCheck(sha, 'pending', 'build', 'build tasks queued'))
-        .then(() =>
-            cfg.build.map((item) =>
-                github.sendStatusCheck(sha, 'pending', `build-${item.name}`, 'build task queued')
-            )
-        )
-        .catch((err) => {
-            github.sendStatusCheck(sha, 'error', 'build', err.cmd || err).then(() => {
-                console.error(`Error occured while initializing the CI process: ${err}`);
-            });
-        });
+    try {
+        await exec(`rm -rf ${process.env.WORK_DIR}/repo`);
+        const cloneCmd =
+            `git clone --single-branch --branch ` +
+            `${pr.head.ref} ${pr.head.repo.ssh_url} ${process.env.WORK_DIR}/repo`;
+        await exec(cloneCmd);
+        const file = fs.readFileSync(`${process.env.WORK_DIR}/repo/github-ci.yml`, 'utf8');
+        cfg = yaml.safeLoad(file);
+
+        if (cfg.env !== null) {
+            await exec(
+                `cp ${process.env.WORK_DIR}/${cfg.env} ${process.env.WORK_DIR}/repo/${cfg.env}`
+            );
+        }
+        await github.sendStatusCheck(sha, 'pending', 'build', 'build tasks queued');
+        await cfg.build.map((item) =>
+            github.sendStatusCheck(sha, 'pending', `build-${item.name}`, 'build task queued')
+        );
+    } catch (err) {
+        await github.sendStatusCheck(sha, 'error', 'build', err.cmd || err);
+        console.error(`Error occured while initializing the CI process: ${err}`);
+    }
 
     // build components one by one
-    return promise.then(() => {
-        for (let i = 0; i < cfg.build.length; i++) {
-            const context = `build-${cfg.build[i].name}`;
-            promise = promise
-                .then(() => exec(`cd ${process.env.WORK_DIR}/repo && ${cfg.build[i].command}`))
-                .then(() => github.sendStatusCheck(sha, 'success', context, 'build successful'))
-                .catch((err) => {
-                    github.sendStatusCheck(sha, 'error', context, err.cmd || err).then(() => {
-                        console.error(`Error occured while building ${cfg.build[i].name}: ${err}`);
-                    });
-                });
+    for (let i = 0; i < cfg.build.length; i++) {
+        const context = `build-${cfg.build[i].name}`;
+        try {
+            await exec(`cd ${process.env.WORK_DIR}/repo && ${cfg.build[i].command}`);
+            await github.sendStatusCheck(sha, 'success', context, 'build successful');
+        } catch (err) {
+            await github.sendStatusCheck(sha, 'error', context, err.cmd || err);
+            console.error(`Error occured while building ${cfg.build[i].name}: ${err}`);
         }
+    }
 
-        // set 'build' status check as 'success' after all components have been successfully built
-        promise
-            .then(() => {
-                github.sendStatusCheck(sha, 'success', 'build', 'build successful').then(() => {
-                    console.log(`Finished processing the PR: ${pr['url']}`);
-                });
-            })
-            .catch((err) => {
-                github.sendStatusCheck(sha, 'error', 'build', err.cmd || err).then(() => {
-                    console.error(`Error occured during the build process: ${err}`);
-                });
-            });
-    });
+    // set 'build' status check as 'success' after all components have been successfully built
+    try {
+        await github.sendStatusCheck(sha, 'success', 'build', 'build successful');
+        console.log(`Finished processing the PR: ${pr['url']}`);
+    } catch (err) {
+        await github.sendStatusCheck(sha, 'error', 'build', err.cmd || err);
+        console.error(`Error occured during the build process: ${err}`);
+    }
 });
 
 // handle invalid routes
