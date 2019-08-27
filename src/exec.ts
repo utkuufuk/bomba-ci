76fc5ab2d8aee5d55470703f90d7cb65241bd188
Utku Ufuk <utkuufuk@gmail.com>
2019-08-27 08:18:18 +0000

    fix log file path

--- src/exec.ts
+++ src/exec.ts
@@ -1,16 +1,16 @@
 import child_process from 'child_process';
 import log, {getLogFilePath} from './log';
+import path from 'path';
 
 const exec = (cmd: string): Promise<void> => {
     log.info(`Running command: ${cmd}\n`);
-    cmd = `${cmd} >> ${getLogFilePath()}`;
+    cmd = `${cmd} >> ${path.resolve(__dirname, '..')}/${getLogFilePath()}`;
     return new Promise((resolve, reject) => {
         child_process.exec(cmd, (error, stdout, stderr) => {
             if (error) {
-                // stderr && log.error(stderr);
+                stderr && log.error(stderr);
                 reject(error);
             }
-            // stdout && log.info(stdout);
             resolve();
         });
     });
